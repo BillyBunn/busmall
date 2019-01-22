a5401ea660359eb6b120887a8d3959696fbd8c06
Billy Bunn <williammbunn@gmail.com>
2019-01-21 16:01:33 -0800

    adds object constructor and render function

---
+++ app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// GLOBAL VARIABLES
+var allImgs = [];
+var imgOne = document.getElementById('image-one');
+var imgTwo = document.getElementById('image-two');
+var imgThree = document.getElementById('image-three');
+
+
+// IMG CONSTRUCTOR FUNCTION
+function Img(name) {
+  // images/sassy-goat.jpg
+  // this.fileExtension = this.src.split('.').pop(); //grabs file extension after '.'
+  this.filepath = `img/${name}`;
+  this.name = name;
+  this.views = 0;
+  this.clicks = 0;
+  allImgs.push(this);
+}
+
+// IMG INSTANCES
+new Img('bag.jpg');
+new Img('banana.jpg');
+new Img('bathroom.jpg');
+new Img('boots.jpg');
+new Img('breakfast.jpg');
+new Img('bubblegum.jpg');
+new Img('chair.jpg');
+new Img('cthulhu.jpg');
+new Img('dog-duck.jpg');
+new Img('dragon.jpg');
+new Img('pen.jpg');
+new Img('pet-sweep.jpg');
+new Img('scissors.jpg');
+new Img('shark.jpg');
+new Img('sweep.jpg');
+new Img('tauntaun.jpg');
+new Img('unicorn.jpg');
+new Img('usb.gif');
+new Img('water-can.jpg');
+new Img('wine-glass.jpg');
+
+
+// RENDER FUNCTION
+function render() {
+  imgOne.src = allImgs[0].filepath;
+  imgTwo.src = allImgs[1].filepath;
+  imgThree.src = allImgs[2].filepath;
+}
+
+render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
