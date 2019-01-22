4754e20e1576c59dc5c2d981e82616f3f0a90f65
Billy Bunn <williammbunn@gmail.com>
2019-01-21 17:03:24 -0800

    turns off event listeners at 25 clicks

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 // GLOBAL VARIABLES
 var allImgs = [];
+var totalClicks = 0;
 var imgOne = document.getElementById('image-one');
 var imgTwo = document.getElementById('image-two');
 var imgThree = document.getElementById('image-three');
@@ -44,7 +45,7 @@ new Img('wine-glass.jpg');
 // RENDER FUNCTION
 function render() {
   getRandom();
-  console.log(randomOne, randomTwo, randomThree);
+  // console.log(randomOne, randomTwo, randomThree);
   imgOne.src = allImgs[randomOne].filepath;
   imgTwo.src = allImgs[randomTwo].filepath;
   imgThree.src = allImgs[randomThree].filepath;
@@ -52,9 +53,6 @@ function render() {
   allImgs[randomTwo].views++;
   allImgs[randomThree].views++;
 }
-
-// console.table(allImgs);
-render();
 
 // GIVES YOU A RANDOM INDEX NUMBER FOR allImgs ARRAY
 var randomOne;
@@ -71,19 +69,56 @@ function getRandom() {
   }
 }
 
-imgOne.addEventListener('click', handleClick);
-imgTwo.addEventListener('click', handleClick);
-imgThree.addEventListener('click', handleClick);
+// CLICK EVENT LISTENERS
+imgOne.addEventListener('click', handleClickOne);
+imgTwo.addEventListener('click', handleClickTwo);
+imgThree.addEventListener('click', handleClickThree);
 
+// REMOVES LISTENERS AFTER 25TH CLICK
+function removeLiseners() {
+if (totalClicks >= 25) {
+    imgOne.removeEventListener('click', handleClickOne);
+    imgTwo.removeEventListener('click', handleClickTwo);
+    imgThree.removeEventListener('click', handleClickThree);
+    console.log('removed click event listeners');
+  }
+}
 
-function handleClick(event) {
+// CLICK EVENT HANDLERS - ONE FOR EACH IMG POSITION
+function handleClickOne(event) {
   console.log(event.target);
+  allImgs[randomOne].clicks++;
+  totalClicks++;
+  // console.log(allImgs[randomOne].clicks);
+  // logClicks();
+  console.log('total clicks:', totalClicks);
+  removeLiseners();
   render();
 }
 
+function handleClickTwo(event) {
+  console.log(event.target);
+  allImgs[randomTwo].clicks++;
+  totalClicks++;
+  // console.log(allImgs[randomThree].clicks);
+  // logClicks();
+  console.log('total clicks:', totalClicks);
+  removeLiseners();
+  render();
+}
 
+function handleClickThree(event) {
+  console.log(event.target);
+  allImgs[randomThree].clicks++;
+  totalClicks++;
+  // console.log(allImgs[randomThree].clicks);
+  // logClicks();
+  console.log('total clicks:', totalClicks);
+  removeLiseners();
+  render();
+}
 
-
+render();
 
 
 
